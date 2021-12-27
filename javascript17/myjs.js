3c64dfaca4489a2912b912cadf91ad99541e040b
範雷雷 <95039519+leileifan166@users.noreply.github.com>
2021-12-27 10:31:39 +0900

    Add files via upload

---
+++ javascript17/myjs.js
@@ -0,0 +1,16 @@
+let ma = "马";
+let xiang = "象";
+let zu = "卒";
+document.write(ma + '走日, ' + xiang + '走田, ' + '小' + zu + '一去不回还<br>');
+
+let years = 2017;
+let months = 10;
+let date = 8;
+let qian = 88.8;
+alert('尊敬的客户:\n截止至' + 
+years + '年' + months + '月' + 
+date + '日,\n' + '您的话费余额为' +
+qian + '元。\n感谢您的使用，详情请、\n登录www.10000.com\n网站进行查询。');
+
+
+
